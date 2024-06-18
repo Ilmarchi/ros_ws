d36c8fada0ff883eb3edb1e833c0ada06ee666a0
Ilmarchi <riccardo.marchi2006@gmail.com>
2024-06-18 10:24:41 +0200

    Init

---
+++ build/learn_topic/CMakeFiles/turtle_pose_subscriber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/dofbot/ros_ws/devel/lib/learn_topic/turtle_pose_subscriber"
+  "/home/dofbot/ros_ws/devel/lib/learn_topic/turtle_pose_subscriber.pdb"
+  "CMakeFiles/turtle_pose_subscriber.dir/src/turtle_pose_subscriber.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtle_pose_subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
